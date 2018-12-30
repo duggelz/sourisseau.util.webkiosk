00734b400936956ca02cddc242d14f5d2b0bf2d2
Douglas Greiman <duggelz@gmail.com>
2018-12-29 22:16:03 -0800

    Various updates
    * Convert from Python 2 to 3
    * Replace hardcoded constants with JSON configuration file

--- WebKioskImageUpdate.py
+++ WebKioskImageUpdate.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Generate thumbnails for WebKiosk and transfer them to a remote server
 #
@@ -14,41 +14,31 @@
 #------------------------------------------------------------------------------
 
 import ctypes
+import datetime
 import glob
+import json
 import os
+import pprint
 import re
+import socket
 import subprocess
-import _winreg
+import winreg
 
 #------------------------------------------------------------------------------
-# Constants
+# Global Variables
 #------------------------------------------------------------------------------
 
-# List of globs to find source images in
-SOURCE_PATHSPECS = [
-    'y:/Edith Smith Collections/Images/*/*/*.tif',
-    'y:/Sourisseau Collections/Photo Collection/Images/*/*/*.tif',
-]
+CONFIG_FN = './WebKioskImageUpdate.json'
 
-# Directory tree to store generated images into
-DEST_ROOT = r'.\Media'
-
-# Transform source filename to dest (relative) filename
+# Transform source filename to dest (relative) filename to make
+# WebKiosk happy, by removing all subdirectory information and
+# collecting all image files in a single directory.
 WK_MANGLE = (
     r'^.*\\Images\\(.*)[.]tif$',
     r'\1',
     1,
     re.IGNORECASE
 )
-
-# Maximum size of longest edge of image, in pixels
-ACCESS_SIZE_LIMIT='1024x1024'
-PREVIEW_SIZE_LIMIT='300x300'
-THUMBNAIL_SIZE_LIMIT='128x128'
-
-#------------------------------------------------------------------------------
-# Global Variables
-#------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
 # Classes/Functions
@@ -78,103 +68,138 @@ def DisableErrorPopup():
         SEM_NOALIGNMENTFAULTEXCEPT|
         SEM_NOOPENFILEERRORBOX
         )
-#
+
+
+def ReadConfig():
+    """Read the config file and return a configuration object.
+
+    Per-host options override global options.
+
+    Config members:
+        source_pathspecs: List of globs to find source images in
+        dest_root: Directory tree to store generated images into
+
+        access_size_limit: Maximum size of access image, in pixels
+        preview_size_limit: Maximum size of preview image, in pixels
+        thumbnail_size_limit: Maximum size of thumbnail image, in pixels
+
+        watermark_filename: Filename to store watermark image in
+        watermark_create_args: ImageMagick command line for creating watermark image
+        watermark_create_args: ImageMagick command line to applying watermark to another image
+
+    Returns the configuration object
+    """
+    print("Reading configuration from %s" % CONFIG_FN)
+    with open(CONFIG_FN, 'r') as config_file:
+        whole_config = json.load(config_file)
+
+    # join global and per-host configs
+    current_config = whole_config['global_config']
+    hostname = socket.gethostname()
+    host_config = whole_config['host_config'][hostname]
+    current_config.update(host_config)
+
+    # Turn into an object with named members
+    config = type('Config', (object,), current_config)
+    print("Configuration:")
+    pprint.pprint({
+        k:v for k,v in vars(config).items()
+        if k[0:1] != '_'})
+    return config
+
 
 def ImageMagickPath(tool, cache={}):
     """Find path to named ImageMagick executable."""
     if tool not in cache:
-        with _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE,
-                             r'Software\\ImageMagick\\Current',
-                             0,
-                             (_winreg.KEY_READ + _winreg.KEY_WOW64_64KEY)) as key:
-            value =  _winreg.QueryValueEx(key, "BinPath")[0]
+        with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,
+                            r'Software\\ImageMagick\\Current',
+                            0,
+                            (winreg.KEY_READ + winreg.KEY_WOW64_64KEY)) as key:
+            value =  winreg.QueryValueEx(key, 'BinPath')[0]
             cache[tool] = os.path.join(value, tool + '.exe')
-        #
-    #
     return cache[tool]
-#
+
 
 def WKManglePath(source_fn):
-    """Return relative filename expected by WebKiosk, given a filename from EmbARK"""
-    wk_fn = re.sub(WK_MANGLE[0], WK_MANGLE[1], source_fn, WK_MANGLE[2], WK_MANGLE[3])
+    """Return relative filename expected by WebKiosk, given a filename from EmbARK
+
+    Returns None if filename format is unrecognized"""
+    wk_fn = re.sub(WK_MANGLE[0], WK_MANGLE[1], source_fn,
+                   WK_MANGLE[2], WK_MANGLE[3])
     if wk_fn == source_fn:
         return None
     else:
         return wk_fn
-    #
-#
+
 
 def NeedsUpdate(fn1, fn2):
-    """Returns True if file2 is older than file1 or this program, or file2
-       doesn't exist"""
+    """Determine if an image should be (re)generated.
+
+    Args:
+        fn1: Filename of source image (file1)
+        fn2: Filename of generated destination image (file2)
+
+    fn1 may be None, in which case it is treated as infinitely old.
+
+    Returns True if file2 doesn't exist or
+            if file2 is older than file1 or this program
+    """
+    # File2 doesn't exist
     if not os.path.isfile(fn2):
         return True
-    #
+
+    # File2 older than File1
     mtime2 = os.path.getmtime(fn2)
     if fn1:
         mtime1 = os.path.getmtime(fn1)
         if mtime2 < mtime1:
             return True
-        #
-    #
+
+    # File2 older than this program
     mtime_program = os.path.getmtime(__file__)
     if mtime2 < mtime_program:
         return True
-    #
     return False
-#
 
-def GetWatermark():
+
+def GetWatermark(config):
     """Return filename of watermark image, generating file if needed"""
-    watermark_fn = os.path.join(DEST_ROOT, 'watermark.tif')
+    watermark_fn = os.path.join(config.dest_root, config.watermark_filename)
     if NeedsUpdate(None, watermark_fn):
-        print "Updating watermark %s" % watermark_fn
-        args = [
-            ImageMagickPath('convert'),
-            #'-bench', '2',
-            #'-antialias',
-            '-size', '4096x4096',
-            '-background', 'none',
-            '-stroke', 'black',
-            '-virtual-pixel', 'tile',
-            '-gravity', 'center',
-            '-fill', 'grey75',
-            '-font', 'Arial',
-            'label: Sourisseau Academy for State and Local History ',
-            '-define', 'distort:viewport=1024x1024-512-512',
-            '-distort', 'Arc', '360 90 1300',
-            watermark_fn,
-        ]
+        print('Updating watermark %s' % watermark_fn)
+        args = [ImageMagickPath('convert')] + config.watermark_create_args + [watermark_fn]
 
         try:
-            if not os.path.isdir(os.path.dirname(watermark_fn)):
-                os.makedirs(os.path.dirname(watermark_fn))
-            #print args
+            print("    Running %s" % args)
+            os.makedirs(os.path.dirname(watermark_fn), exist_ok=True)
             output = subprocess.check_output(args, stderr=subprocess.STDOUT)
             if output.strip():
-                print output
-            #
-        except (IOError, WindowsError, subprocess.CalledProcessError), e:
-            print "Error generating watermark %s: %s" % (watermark_fn, str(e))
-            return
-        #
-    #
+                print(output)
+        except (IOError, WindowsError, subprocess.CalledProcessError) as e:
+            print('  Error generating watermark: %s' % (e,))
+            raise # This is a fatal error
     return watermark_fn
-#
 
-def UpdateDestImage(source_fn, stats, wk_rel_path, wk_ext, dest_dir, size_limit, watermark=False):
-    """Generate a single access, preview, or thumbnail image"""
-    dest_fn = os.path.join(DEST_ROOT, dest_dir, wk_rel_path + wk_ext)
-    #print dest_access_fn, dest_preview_fn, dest_thumbnail_fn
 
+def UpdateDestImage(config, stats, source_fn, dest_fn, size_limit, watermark):
+    """Generate a single access, preview, or thumbnail image
+
+    Args:
+        config: Config object
+        stats: Cumulative statistics (updated by this function)
+        source_fn: Filename of archival source image
+        dest_fn: Filename of generated image
+        size_limit: Maximum size of generated images (e.g. '1024x1024')
+        watermark: Apply watermark to generated image if True, else don't
+    """
+    print('Considering generated file %s' % dest_fn)
     if not NeedsUpdate(source_fn, dest_fn):
         stats.skipped += 1
-        print "Skipping %s, up-to-date" % dest_fn
+        print('  Skipping, up-to-date')
         return
-    #
 
     stats.updated += 1
-    print "Updating %s from %s" % (dest_fn, source_fn)
+    print('  Updating from %s' % (source_fn,))
 
     args = [
         ImageMagickPath('convert'),
@@ -183,63 +208,74 @@ def UpdateDestImage(source_fn, stats, wk_rel_path, wk_ext, dest_dir, size_limit,
         '-thumbnail', size_limit,
         ]
     if watermark:
-        watermark_fn = GetWatermark()
-        args.extend([
-            watermark_fn,
-            '-gravity', 'center',
-            '-compose', 'dissolve', '-define', 'compose:args=30,100',
-            '-composite',
-        ])
-    #
+        watermark_fn = GetWatermark(config)
+        args.extend([watermark_fn])
+        args.extend(config.watermark_apply_args)
     args.extend(['JPEG:' + dest_fn])
 
     try:
-        if not os.path.isdir(os.path.dirname(dest_fn)):
-            os.makedirs(os.path.dirname(dest_fn))
-        #
-        #print args
+        print("    Running %s" % args)
+        os.makedirs(os.path.dirname(dest_fn), exist_ok=True)
         output = subprocess.check_output(args, stderr=subprocess.STDOUT)
         if output.strip():
-            print output
-        #
-    except (IOError, WindowsError,subprocess.CalledProcessError), e:
-        print "Error generating thumbnail image %s: %s" % (dest_fn, str(e))
+            print(output)
+    except (IOError, WindowsError,subprocess.CalledProcessError) as e:
+        print('  Error generating image: %s' % (e,))
         return
-    #
-#
 
-def UpdateFromSourceImage(source_fn, stats):
+
+def UpdateFromSourceImage(config, stats, source_fn):
     """Generate access, preview, and thumbnail images from single source image"""
+    print('Considering source image %s' % (source_fn,))
+    # Webkiosk wants the images in different directories than EmbARK
     wk_rel_path = WKManglePath(source_fn)
     if not wk_rel_path:
         stats.unrecognized += 1
-        print "Skipping %s, path not recognized" % source_fn
+        print('  Skipping, path not recognized')
         return
-    #
-    #print source_fn, wk_rel_path
-    UpdateDestImage(source_fn, stats, wk_rel_path, '.tif', "Images", ACCESS_SIZE_LIMIT, watermark=True)
-    UpdateDestImage(source_fn, stats, wk_rel_path, '.jpg', "Previews", PREVIEW_SIZE_LIMIT)
-    UpdateDestImage(source_fn, stats, wk_rel_path, '.jpg', "Thumbnails", THUMBNAIL_SIZE_LIMIT)
-#
 
-def UpdateAll(pathspecs):
+    UpdateDestImage(config, stats, source_fn,
+                    os.path.join(config.dest_root, 'Images', wk_rel_path + '.tif'),
+                    config.access_size_limit, watermark=True)
+    UpdateDestImage(config, stats, source_fn,
+                    os.path.join(config.dest_root, 'Previews', wk_rel_path + '.jpg'),
+                    config.preview_size_limit, watermark=False)
+    UpdateDestImage(config, stats, source_fn,
+                    os.path.join(config.dest_root, 'Thumbnails', wk_rel_path + '.jpg'),
+                    config.thumbnail_size_limit, watermark=False)
+
+
+def UpdateAll(config):
     """Find all EmbARK images, and generate all Web Kiosk images from them"""
+
+    # Collect cumulative statistics
     stats = type('Stats', (object,), { 'found':0, 'unrecognized':0, 'skipped':0, 'updated':0})
-    for pathspec in pathspecs:
+
+    # Walk the source trees
+    for pathspec in config.source_pathspecs:
         for source_fn in glob.iglob(pathspec):
             source_fn = os.path.abspath(source_fn)
             stats.found += 1
-            UpdateFromSourceImage(source_fn, stats)
-        #
-    #
-    print "Found %d source images (%d unrecognized)" % (stats.found, stats.unrecognized)
-    print "Updated %d destination images, skipped %d already up-to-date" % (stats.updated, stats.skipped)
-#
+            UpdateFromSourceImage(config, stats, source_fn)
+
+    # Print summary
+    summary = """
+Summary:
+
+Found %d source images
+  (%d unrecognized)
+Updated %d destination images
+Skipped %d already up-to-date
+
+Done""" % (
+    stats.found, stats.unrecognized, stats.updated, stats.skipped)
+    print(summary)
 
 def main():
-    GetWatermark()
-    UpdateAll(SOURCE_PATHSPECS)
-#
+    print("Starting WebKioskImageUpdate.py at %s" % (datetime.datetime.now().isoformat(),))
+    config = ReadConfig()
+    GetWatermark(config)
+    UpdateAll(config)
 
 #------------------------------------------------------------------------------
 # Main
@@ -248,4 +284,3 @@ def main():
 if __name__ == '__main__':
     DisableErrorPopup()
     main()
-#
